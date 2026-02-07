fb787891877c8f7bcbf6458f4adfcef77e6d575a
omby <randyombiro8@gmail.com>
2026-02-07 21:21:17 +0300

    Add Place constructor to script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+//place constructor
+ function Place(location,landmarks,timeOfYear,notes){
+    this.location = location;
+    this.landmarks = landmarks;
+    this.timeOfYear  = timeOfYear;
+    this.notes = notes;
+
+ }
+ const place = new place('test',['test'],'test','test');
+ console.log('constructor test:',testPlace);
